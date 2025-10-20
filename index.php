8dd177e6713eafd804d27bd34b875e1cb1edd84b
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-10-20 10:19:08 +0200

    Funciones Aç

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "funciones.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios funciones 1</title>
+</head>
+<body>
+    <h1>Funcion suma</h1>
+    <h2><?= sumar(10,20)?></h2>
+    <h1>Funcion saludar</h1>
+    <h2><?= saludar("Rocio")?></h2>
+    <h1>Funcion swap</h1>
+    <h2><?= swap(25,8)?></h2>
+</body>
+</html>
